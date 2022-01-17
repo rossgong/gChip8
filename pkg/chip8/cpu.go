b1dd1edc74cccd90b3aa50ea92ee2753f769488e
Ross Gongaware <ross@gongaware.org>
2022-01-17 15:14:33 -0500

    Implement decode function for opcodes < 0x9000

--- pkg/chip8/cpu.go
+++ pkg/chip8/cpu.go
@@ -44,12 +44,12 @@ func (cpu *CPU) Cycle() error {
 	}
 
 	//decode
-	op, err := cpu.decode(opcode)
+	cpu.execute, err = cpu.decode(opcode)
 	if err == nil {
 		return err
 	}
 	//execute
-	op()
+	cpu.execute()
 	return nil
 }
 
@@ -61,23 +61,39 @@ func (cpu *CPU) fetch() (Instruction, error) {
 }
 
 func (cpu *CPU) decode(opcode Instruction) (Operation, error) {
-	nibble1 := opcode & 0xF000 //Mask to solo the first nibble
+	startingNibble := opcode & 0xF000 //Mask to solo the first nibble
 
-	switch nibble1 {
+	switch startingNibble {
 	case 0x0000:
 		//TODO: SYS CALLS
 	case 0x1000: //JP instruction
 		return jump(&cpu.programCounter, maskAddress(opcode)), nil
 	case 0x2000: //CALL instruction
 		return subroutineCall(cpu, maskAddress(opcode))
-	case 0x3000: //SE
+	case 0x3000: //SE Skip if register equals byte
 		return skipInstructionIfTrue(&cpu.programCounter,
-			cpu.Registers[maskXRegister(opcode)] == byte(maskEndingByte(opcode))), nil
-	case 0x4000:
-	case 0x5000:
-	case 0x6000:
-	case 0x7000:
-	case 0x8000:
+			cpu.Registers[maskXRegister(opcode)] == maskEndingByte(opcode)), nil
+	case 0x4000: //SNE Skip if register does not equal byte
+		return skipInstructionIfTrue(&cpu.programCounter,
+			cpu.Registers[maskXRegister(opcode)] != maskEndingByte(opcode)), nil
+	case 0x5000: //SE Skip if register equals register
+		return skipInstructionIfTrue(&cpu.programCounter,
+			cpu.Registers[maskXRegister(opcode)] == cpu.Registers[maskYRegister(opcode)]), nil
+	case 0x6000: //LD Load byte into register
+		return loadRegister(&cpu.Registers[maskXRegister(opcode)], maskEndingByte(opcode)), nil
+	case 0x7000: //ADD Adds byte into register
+		xRegister := maskXRegister(opcode)
+		return add(&cpu.Registers[statusRegister], &cpu.Registers[xRegister], cpu.Registers[xRegister]), nil
+	case 0x8000: //Various math functions requires both registers
+		xRegister := &cpu.Registers[maskXRegister(opcode)]
+		yRegister := cpu.Registers[maskYRegister(opcode)]
+		lastNibble := opcode & 0x000F //Mask to solo the last nibble\
+		op := decode8(&cpu.Registers[statusRegister], xRegister, yRegister, lastNibble)
+		if op == nil {
+			return op, fmt.Errorf("decode error: 0x%X not implemented", opcode)
+		} else {
+			return op, nil
+		}
 	case 0x9000:
 	case 0xA000:
 	case 0xB000:
@@ -86,7 +102,32 @@ func (cpu *CPU) decode(opcode Instruction) (Operation, error) {
 	case 0xE000:
 	case 0xF000:
 	}
-	return nil, fmt.Errorf("decode error: not implemented")
+	return nil, fmt.Errorf("decode error: 0x%X not implemented", opcode)
+}
+
+//Function to make decode 0x8xxx not cloud up the decode function
+func decode8(statusRegister *byte, xRegister *byte, yValue byte, lastNibble Instruction) Operation {
+	switch lastNibble {
+	case 0x0000: //LD Load register Y into register X
+		return loadRegister(xRegister, yValue)
+	case 0x0001: //OR Store registerX OR registerY into register X
+		return or(xRegister, yValue)
+	case 0x0002: //AND Store registerX AND registerY into register X
+		return and(xRegister, yValue)
+	case 0x0003: //XOR Store registerX XOR registerY into register X
+		return xor(xRegister, yValue)
+	case 0x0004: //ADD Store registerX + registerY into register X
+		return add(statusRegister, xRegister, yValue)
+	case 0x0005: //SUB Store registerX - registerY into register X
+		return subtract(statusRegister, xRegister, yValue)
+	case 0x0006: //SHR Store registerX >> 1 into register X
+		return shiftRight(statusRegister, xRegister)
+	case 0x0007: //SUBN Store registerY - registerX into register X
+		return subtractN(statusRegister, xRegister, yValue)
+	case 0x000E: //SHL Store registerX << 1 into register X
+		return shiftLeft(statusRegister, xRegister)
+	}
+	return nil
 }
 
 /*
@@ -99,44 +140,35 @@ func maskAddress(opcode Instruction) Address {
 	return Address(opcode & 0x0FFF)
 }
 
-func maskXRegister(opcode Instruction) uint8 {
-	return uint8((opcode & 0x0F00) >> 8) //Mask corrrect nibble and then shift and convert
+func maskXRegister(opcode Instruction) byte {
+	return byte((opcode & 0x0F00) >> 8) //Mask corrrect nibble and then shift and convert
 }
 
-func maskYRegister(opcode Instruction) uint8 {
-	return uint8((opcode & 0x00F0) >> 4) //Mask corrrect nibble and then shift and convert
+func maskYRegister(opcode Instruction) byte {
+	return byte((opcode & 0x00F0) >> 4) //Mask corrrect nibble and then shift and convert
 }
 
-func maskEndingByte(opcode Instruction) uint8 {
-	return uint8(opcode & 0x00FF)
+func maskEndingByte(opcode Instruction) byte {
+	return byte(opcode & 0x00FF)
 }
 
 //Error functions
-func checkOutOfBounds(fnString string, indicies ...uint8) error {
-	hasError := false
-	fnString += " ("
-	for i, reg := range indicies {
-		if reg >= registerCount {
-			if hasError { //If there is already an invlid register
-				fnString += fmt.Sprintf("&&|V%v", rune(byte('x')+byte(i)))
-			} else {
-				fnString += fmt.Sprintf("|V%s", rune(byte('x')+byte(i)))
-			}
-			hasError = true
-		}
-	}
-	if hasError {
-		return fmt.Errorf("%s): invalid register number", fnString)
-	} else {
-		return nil
-	}
-}
-
-func checkRegistersAndReturnFunction(fnString string, function Operation, registers ...uint8) (Operation, error) {
-	err := checkOutOfBounds(fnString, registers...)
-	if err == nil {
-		return function, nil
-	} else {
-		return nil, err
-	}
-}
+// func checkOutOfBounds(fnString string, indicies ...uint8) error {
+// 	hasError := false
+// 	fnString += " ("
+// 	for i, reg := range indicies {
+// 		if reg >= registerCount {
+// 			if hasError { //If there is already an invlid register
+// 				fnString += fmt.Sprintf("&&|V%v", rune(byte('x')+byte(i)))
+// 			} else {
+// 				fnString += fmt.Sprintf("|V%s", rune(byte('x')+byte(i)))
+// 			}
+// 			hasError = true
+// 		}
+// 	}
+// 	if hasError {
+// 		return fmt.Errorf("%s): invalid register number", fnString)
+// 	} else {
+// 		return nil
+// 	}
+// }
